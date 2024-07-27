df20ab2a79f3d8cef47c801f76706fee2af2e64a
Yuns <yuns.xie@qq.com>
2024-07-27 11:22:15 +0800

    ♻️ chore: drop gogocode

--- test/index.test.ts
+++ test/index.test.ts
@@ -3,7 +3,7 @@
 
 import { describe, expect, it } from 'vitest'
 
-import { transformTags, transformTagsNext } from '../src/helpers/transform'
+import { transformTags } from '../src/helpers/transform'
 
 const clsFixtures = import.meta.glob('./fixtures/cls-tags/*.txt', {
   eager: true,
@@ -27,23 +27,4 @@ describe('cls-tags', () => {
       })
     })
   console.timeEnd('old')
-})
-
-describe('cls-tags next', () => {
-  console.time('next')
-  Object.entries(clsFixtures)
-    .filter(([path]) => {
-      return !path.endsWith('.output.txt')
-    })
-    .forEach(([path, code]) => {
-      it(path, () => {
-        const expectedOutputCode =
-          clsFixtures[path.replace('.txt', '.output.txt')]
-        const polishCode = transformTagsNext(code, {
-          clsTags: ['cls'],
-        })
-        expect(polishCode).equal(expectedOutputCode)
-      })
-    })
-  console.timeEnd('next')
 })
